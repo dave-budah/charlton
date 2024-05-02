7b0c8e8663f2cbe4312451c673aa9dc08ca3d99e
Dave Budah <davebudah@gmail.com>
2024-05-02 08:04:39 +0200

    link colors updated

--- dist/browser/chunk-36NSD5ZI.js
+++
@@ -1,1 +0,0 @@
-import{Aa as o,Ba as a,Ha as r,Ia as s,T as i}from"./chunk-YRXI6WP5.js";var d=(()=>{let e=class e{};e.\u0275fac=function(n){return new(n||e)},e.\u0275cmp=i({type:e,selectors:[["app-signup"]],standalone:!0,features:[s],decls:2,vars:0,template:function(n,c){n&1&&(o(0,"p"),r(1,"signup works!"),a())}});let t=e;return t})();export{d as SignupComponent};
--- dist/browser/chunk-QAHILYEN.js
+++
@@ -1,1 +0,0 @@
-import{Aa as i,Ba as a,Ha as r,Ia as s,T as o}from"./chunk-YRXI6WP5.js";var m=(()=>{let e=class e{};e.\u0275fac=function(n){return new(n||e)},e.\u0275cmp=o({type:e,selectors:[["app-login"]],standalone:!0,features:[s],decls:2,vars:0,template:function(n,l){n&1&&(i(0,"p"),r(1,"login works!"),a())}});let t=e;return t})();export{m as LoginComponent};
